3bad353cbfe32f8fb70a57e754b84a4892bae244
Fuza322 <vanya.osipik322@gmail.com>
2020-10-03 14:26:27 +0300

    Lesson17

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/1024/211/png-transparent-social-media-name-tag-social-web-social-media-angle-logo-social-media.png'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdn.wpbeaveraddons.com/wp-content/uploads/luca-micheli-422052-unsplash-2.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
