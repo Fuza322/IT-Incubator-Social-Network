e627f083726f66cd17b8318db96f3d825226eda9
Fuza322 <vanya.osipik322@gmail.com>
2021-07-20 17:09:48 +0300

    lesson 75 redux-from (Login)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,43 @@
 import React from "react"
+import {reduxForm, Field, InjectedFormProps} from "redux-form"
 
-type LoginPropsType = {
-
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
 }
 
-export const Login = (props: LoginPropsType) => {
+const LoginForm = (props: InjectedFormProps<FormDataType>) => {
     return (
-        <h1>LOGIN</h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field type={"checkbox"} name={"rememberMe"} component={"input"}/> Remember me
+            </div>
+            <div>
+                <button>Log in</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm<FormDataType>({form: "LoginForm"})(LoginForm)
+
+export const Login = () => {
+
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     )
 }
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,13 +4,15 @@ import {profileReducer} from "./profile-reducer"
 import {dialogsReducer} from "./dialogs-reducer"
 import {usersReducer} from "./users-reducer"
 import {authReducer} from "./auth-reducer"
+import {reducer as formReducer} from "redux-form"
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     // sidebar : sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
