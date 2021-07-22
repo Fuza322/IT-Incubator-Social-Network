2b20f072a7d58a2e607cadd7e6d0eb3070ac6b90
Fuza322 <vanya.osipik322@gmail.com>
2021-07-22 23:55:16 +0300

    lesson 83 Selector from 'reselect'

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,20 +1,32 @@
 import {RootStateType} from "./redux-store"
 
+/*export const getUsersSelector = (state: RootStateType) => {
+    return state.usersPage.users
+}
+export const getUsers = createSelector(getUsersSelector, (users: Array<UserType>) => {
+    return users.filter(u => true)
+})*/
+
 export const getUsers = (state: RootStateType) => {
     return state.usersPage.users
 }
+
 export const getPageSize = (state: RootStateType) => {
     return state.usersPage.pageSize
 }
+
 export const getTotalUsersCount = (state: RootStateType) => {
     return state.usersPage.totalUsersCount
 }
+
 export const getCurrentPage = (state: RootStateType) => {
     return state.usersPage.currentPage
 }
+
 export const getIsFetching = (state: RootStateType) => {
     return state.usersPage.isFetching
 }
+
 export const getFollowingInProgress = (state: RootStateType) => {
     return state.usersPage.followingInProgress
 }
